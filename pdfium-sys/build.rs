52823acd86d106945fb9bc3d750c8a93272196ba
Maximilian Luz <luzmaximilian@gmail.com>
2023-03-17 21:35:19 +0100

    pdfium-sys: Keep comments from header files

--- pdfium-sys/build.rs
+++ pdfium-sys/build.rs
@@ -3,32 +3,27 @@ use std::path::PathBuf;
 fn main() {
     let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
 
-    let defines: &[&'static str] = &[
-        #[cfg(feature = "v8")]
-        "PDF_ENABLE_V8",
-
-        #[cfg(feature = "xfa")]
-        "PDF_ENABLE_XFA",
-
-        #[cfg(feature = "skia")]
-        "_SKIA_SUPPORT_",
+    let args: &[&'static str] = &[
+        "-fretain-comments-from-system-headers",
+        "-fparse-all-comments",
+        #[cfg(feature = "v8")] "-DPDF_ENABLE_V8",
+        #[cfg(feature = "xfa")] "-DPDF_ENABLE_XFA",
+        #[cfg(feature = "skia")] "-D_SKIA_SUPPORT_",
     ];
 
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    let mut builder = bindgen::Builder::default()
+    let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .dynamic_library_name("libpdfium")
-        .dynamic_link_require_all(cfg!(feature = "dylib-require-all"));
-
-    for d in defines {
-        builder = builder.clang_arg(format!("-D{d}"));
-    }
-
-    builder
+        .dynamic_link_require_all(cfg!(feature = "dylib-require-all"))
+        .clang_args(args)
+        .generate_comments(true)
         .generate()
-        .expect("Unable to generate pdfium bindings")
+        .expect("Unable to generate pdfium bindings");
+
+    bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
