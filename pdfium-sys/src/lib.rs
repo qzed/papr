e7b5da9ebcf85d6e7016e1897f0305d09941549d
Maximilian Luz <luzmaximilian@gmail.com>
2023-03-19 04:46:41 +0100

    pdfium-sys: Ignore doc warnings

--- pdfium-sys/src/lib.rs
+++ pdfium-sys/src/lib.rs
@@ -1,6 +1,8 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(rustdoc::bare_urls)]
+#![allow(rustdoc::broken_intra_doc_links)]
 #![allow(clippy::all)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
