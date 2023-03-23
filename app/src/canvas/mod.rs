e16dba3c6017bff2c2e5eeb3aa735254506cae89
Maximilian Luz <luzmaximilian@gmail.com>
2023-03-23 14:55:20 +0100

    app: Implement an inefficient tiled rendering approach

--- app/src/canvas/mod.rs
+++ app/src/canvas/mod.rs
@@ -2,7 +2,7 @@ use gtk::traits::SnapshotExt;
 use gtk::Snapshot;
 use gtk::{gdk, glib};
 
-use na::{point, vector, Point2, Similarity2, Translation2, Vector2};
+use na::{point, vector, Similarity2, Translation2, Vector2};
 use nalgebra as na;
 
 use pdfium::bitmap::{Bitmap, BitmapFormat};
@@ -93,51 +93,80 @@ impl Canvas {
                 continue;
             }
 
-            // page offset in display pixels
-            let page_offs_d = page_rect.offs - page_clipped.offs;
+            // draw background
+            snapshot.append_color(&gdk::RGBA::new(1.0, 1.0, 1.0, 1.0), &page_clipped.into());
 
-            // allocate buffer to which the PDF is being rendered
-            let stride = page_clipped.size.x as usize * 4;
-            let mut buffer = vec![0; stride * page_clipped.size.y as usize];
+            // tiled rendering (for now: very inefficient)
+            let tile_size = vector![512, 512];
 
-            // render the PDF page
-            {
-                // wrap buffer in bitmap
-                let mut bmp = Bitmap::from_buf(
-                    page.library().clone(),
-                    page_clipped.size.x as _,
-                    page_clipped.size.y as _,
-                    BitmapFormat::Bgra,
-                    &mut buffer[..],
-                    stride as _,
-                )
-                .unwrap();
+            // viewport bounds relative to the page in pixels (area of page visible on screen)
+            let visible_page = Rect::new(-page_rect.offs, na::convert_unchecked(vp.r.size))
+                .clip(&Rect::new(point![0, 0], page_rect.size))
+                .bounds();
 
-                // set up render layout
-                let layout = PageRenderLayout {
-                    start: na::convert::<_, Vector2<i32>>(page_offs_d).into(),
-                    size: na::convert(page_rect.size),
-                    rotate: PageRotation::None,
-                };
+            // tile bounds
+            let tiles = Bounds {
+                x_min: visible_page.x_min / tile_size.x,
+                y_min: visible_page.y_min / tile_size.y,
+                x_max: (visible_page.x_max + tile_size.x - 1) / tile_size.x,
+                y_max: (visible_page.y_max + tile_size.y - 1) / tile_size.y,
+            };
 
-                // render page to bitmap
-                let flags = RenderFlags::LcdText | RenderFlags::Annotations;
-                page.render(&mut bmp, &layout, flags).unwrap();
+            snapshot.push_clip(&screen_rect.into());
+
+            for ix in tiles.range_x() {
+                for iy in tiles.range_y() {
+                    let tile_offs = vector![ix, iy].component_mul(&tile_size);
+
+                    // allocate tile bitmap buffer
+                    let stride = tile_size.x as usize * 4;
+                    let mut buffer = vec![0; stride * tile_size.y as usize];
+
+                    // render to tile
+                    {
+                        // wrap buffer in bitmap
+                        let mut bmp = Bitmap::from_buf(
+                            page.library().clone(),
+                            tile_size.x as _,
+                            tile_size.y as _,
+                            BitmapFormat::Bgra,
+                            &mut buffer[..],
+                            stride as _,
+                        )
+                        .unwrap();
+
+                        // set up render layout
+                        let layout = PageRenderLayout {
+                            start: na::convert::<_, Vector2<i32>>(-tile_offs).into(),
+                            size: na::convert(page_rect.size),
+                            rotate: PageRotation::None,
+                        };
+
+                        // render page to bitmap
+                        let flags = RenderFlags::LcdText | RenderFlags::Annotations;
+                        page.render(&mut bmp, &layout, flags).unwrap();
+                    }
+
+                    // transfer buffer ownership to GTK/GDK
+                    let bytes = glib::Bytes::from_owned(buffer);
+                    let texture = gdk::MemoryTexture::new(
+                        tile_size.x as _,
+                        tile_size.y as _,
+                        gdk::MemoryFormat::B8g8r8a8,
+                        &bytes,
+                        stride as _,
+                    );
+
+                    // draw background and page contents
+                    let tile_screen_rect = Rect {
+                        offs: page_rect.offs + tile_offs,
+                        size: tile_size,
+                    };
+                    snapshot.append_texture(&texture, &tile_screen_rect.into());
+                }
             }
 
-            // transfer buffer ownership to GTK/GDK
-            let bytes = glib::Bytes::from_owned(buffer);
-            let texture = gdk::MemoryTexture::new(
-                page_clipped.size.x as _,
-                page_clipped.size.y as _,
-                gdk::MemoryFormat::B8g8r8a8,
-                &bytes,
-                stride as _,
-            );
-
-            // draw background and page contents
-            snapshot.append_color(&gdk::RGBA::new(1.0, 1.0, 1.0, 1.0), &page_clipped.into());
-            snapshot.append_texture(&texture, &page_clipped.into());
+            snapshot.pop();
         }
     }
 }
