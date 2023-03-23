43820ab37a80d0808b79612fde40793a313f7bd5
Maximilian Luz <luzmaximilian@gmail.com>
2023-03-23 04:17:56 +0100

    app: Add more functions for helper types

--- app/src/types.rs
+++ app/src/types.rs
@@ -1,9 +1,9 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add, Range, Sub};
 
 use gtk::graphene;
 use nalgebra::{point, vector};
 use nalgebra::{Point2, Scalar, Vector2};
-use num_traits::{Zero, Float};
+use num_traits::{Float, Zero};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Bounds<T> {
@@ -13,8 +13,11 @@ pub struct Bounds<T> {
     pub y_max: T,
 }
 
-impl<T: num_traits::Zero> Bounds<T> {
-    pub fn zero() -> Self {
+impl<T> Bounds<T> {
+    pub fn zero() -> Self
+    where
+        T: Zero,
+    {
         Self {
             x_min: T::zero(),
             y_min: T::zero(),
@@ -22,16 +25,48 @@ impl<T: num_traits::Zero> Bounds<T> {
             y_max: T::zero(),
         }
     }
+
+    pub fn rect(&self) -> Rect<T>
+    where
+        T: Copy,
+        T: Scalar,
+        T: Sub<T, Output = T>,
+    {
+        Rect {
+            offs: point![self.x_min, self.y_min],
+            size: vector![self.x_max - self.x_min, self.y_max - self.y_min],
+        }
+    }
+
+    pub fn range_x(&self) -> Range<T>
+    where
+        T: Copy,
+    {
+        (self.x_min)..(self.x_max)
+    }
+
+    pub fn range_y(&self) -> Range<T>
+    where
+        T: Copy,
+    {
+        (self.y_min)..(self.y_max)
+    }
+}
+
+impl<T> From<Rect<T>> for Bounds<T>
+where
+    T: Copy,
+    T: Scalar,
+    T: Add<T, Output = T>,
+{
+    fn from(r: Rect<T>) -> Self {
+        r.bounds()
+    }
 }
 
 impl From<Bounds<f32>> for graphene::Rect {
     fn from(b: Bounds<f32>) -> Self {
-        graphene::Rect::new(
-            b.x_min,
-            b.y_min,
-            b.x_max - b.x_min,
-            b.y_max - b.y_min,
-        )
+        graphene::Rect::new(b.x_min, b.y_min, b.x_max - b.x_min, b.y_max - b.y_min)
     }
 }
 
@@ -78,8 +113,8 @@ impl<T: Scalar> Rect<T> {
     where
         T: Copy,
         T: Ord,
-        T: Add<T, Output=T>,
-        T: Sub<T, Output=T>,
+        T: Add<T, Output = T>,
+        T: Sub<T, Output = T>,
     {
         let offs = point![self.offs.x.max(other.offs.x), self.offs.y.max(other.offs.y)];
         let size = vector![
@@ -99,38 +134,63 @@ impl<T: Scalar> Rect<T> {
             size: vector![self.size.x.round(), self.size.y.round()],
         }
     }
+
+    pub fn bounds(&self) -> Bounds<T>
+    where
+        T: Copy,
+        T: Add<T, Output = T>,
+    {
+        Bounds {
+            x_min: self.offs.x,
+            y_min: self.offs.y,
+            x_max: self.offs.x + self.size.x,
+            y_max: self.offs.y + self.size.y,
+        }
+    }
+
+    pub fn range_x(&self) -> Range<T>
+    where
+        T: Copy,
+        T: Add<T, Output = T>,
+    {
+        (self.offs.x)..(self.offs.y + self.size.y)
+    }
+
+    pub fn range_y(&self) -> Range<T>
+    where
+        T: Copy,
+        T: Add<T, Output = T>,
+    {
+        (self.offs.y)..(self.offs.y + self.size.y)
+    }
+}
+
+impl<T> From<Bounds<T>> for Rect<T>
+where
+    T: Copy,
+    T: Scalar,
+    T: Sub<T, Output = T>,
+{
+    fn from(b: Bounds<T>) -> Self {
+        b.rect()
+    }
 }
 
 impl From<Rect<f32>> for graphene::Rect {
     fn from(r: Rect<f32>) -> Self {
-        graphene::Rect::new(
-            r.offs.x,
-            r.offs.y,
-            r.size.x,
-            r.size.y,
-        )
+        graphene::Rect::new(r.offs.x, r.offs.y, r.size.x, r.size.y)
     }
 }
 
 impl From<Rect<f64>> for graphene::Rect {
     fn from(r: Rect<f64>) -> Self {
-        graphene::Rect::new(
-            r.offs.x as _,
-            r.offs.y as _,
-            r.size.x as _,
-            r.size.y as _,
-        )
+        graphene::Rect::new(r.offs.x as _, r.offs.y as _, r.size.x as _, r.size.y as _)
     }
 }
 
 impl From<Rect<i64>> for graphene::Rect {
     fn from(r: Rect<i64>) -> Self {
-        graphene::Rect::new(
-            r.offs.x as _,
-            r.offs.y as _,
-            r.size.x as _,
-            r.size.y as _,
-        )
+        graphene::Rect::new(r.offs.x as _, r.offs.y as _, r.size.x as _, r.size.y as _)
     }
 }
 
