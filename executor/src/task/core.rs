b1073d96db0c8374b1502d2335f4ad9a14a861ea
Maximilian Luz <luzmaximilian@gmail.com>
2023-04-10 16:12:27 +0200

    executor: Add comments for core task structs

--- executor/src/task/core.rs
+++ executor/src/task/core.rs
@@ -6,6 +6,7 @@ use super::state::State;
 use super::vtable;
 use super::vtable::Vtable;
 
+/// Full task structure, encompassing all task data.
 pub struct Cell<T, F, R> {
     /// Common task state and data without any specific type references.
     pub(super) header: Header,
@@ -14,6 +15,9 @@ pub struct Cell<T, F, R> {
     pub(super) core: Core<T, F, R>,
 }
 
+/// Task header.
+///
+/// Type-independent data common to all tasks.
 pub struct Header {
     /// Current state of this task.
     pub(super) state: State,
@@ -26,6 +30,10 @@ pub struct Header {
     pub(super) vtable: &'static Vtable,
 }
 
+/// Core task data.
+///
+/// Stores the type-secific task data (i.e., closure or result, depending on
+/// the stage) and the task adapter.
 pub struct Core<T, F, R> {
     /// Stage specific data.
     pub(super) data: UnsafeCell<Data<F, R>>,
@@ -34,6 +42,10 @@ pub struct Core<T, F, R> {
     pub(super) adapter: T,
 }
 
+/// Stage-specific task data.
+///
+/// Stores either nothing, the closure to be executed, the result of that
+/// closure, or any panic that occurred when the closure was run.
 #[derive(Default)]
 pub enum Data<F, R> {
     /// Empty variant, storing no stage-specific data.
