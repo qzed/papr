d50a406f63259218bb490a8783ff651fc32c8201
Maximilian Luz <luzmaximilian@gmail.com>
2023-04-19 01:41:50 +0200

    app: Use jemalloc to improve memory usage
    Some informal testing shows reduced memory usage with jemalloc as
    opposed to the default allocator. This can likely be attributed to heap
    fragmentation. So let's use jemalloc instead.

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 use gtk::{glib, prelude::ApplicationExtManual};
 
 mod canvas;
