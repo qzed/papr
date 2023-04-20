37a6ade13067c48f9a0f4af2f00248bf4c7f7b65
Maximilian Luz <luzmaximilian@gmail.com>
2023-04-20 15:30:38 +0200

    app: Add helper for tile ID access

--- app/src/canvas/tile/mod.rs
+++ app/src/canvas/tile/mod.rs
@@ -1,6 +1,8 @@
 mod scheme;
 pub use scheme::{ExactLevelTilingScheme, HybridTilingScheme, QuadTreeTilingScheme, TilingScheme};
 
+use nalgebra::{point, Point2};
+
 use crate::types::Bounds;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -15,6 +17,11 @@ impl TileId {
     #[inline]
     pub fn new(page: usize, x: i64, y: i64, z: i64) -> Self {
         Self { page, x, y, z }
+    }
+
+    #[inline]
+    pub fn xy(&self) -> Point2<i64> {
+        point![self.x, self.y]
     }
 }
 
