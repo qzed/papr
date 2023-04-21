6e8ff4926ecc9885e3da4effad5c8fd00bb85e19
Maximilian Luz <luzmaximilian@gmail.com>
2023-04-21 14:38:30 +0200

    app: Move tile structs to their own module

--- app/src/canvas/render/mod.rs
+++ app/src/canvas/render/mod.rs
@@ -1,6 +1,8 @@
-use nalgebra::{point, Point2};
+mod fallback;
+pub use fallback::{FallbackManager, FallbackSpec};
 
-use crate::types::Bounds;
+mod manager;
+pub use manager::TileManager;
 
 mod scheme;
 pub use scheme::{ExactLevelTilingScheme, HybridTilingScheme, QuadTreeTilingScheme, TilingScheme};
@@ -8,33 +10,5 @@ pub use scheme::{ExactLevelTilingScheme, HybridTilingScheme, QuadTreeTilingSchem
 mod source;
 pub use source::{TileHandle, TilePriority, TileSource};
 
-mod manager;
-pub use manager::TileManager;
-
-mod fallback;
-pub use fallback::{FallbackManager, FallbackSpec};
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct TileId {
-    pub page: usize,
-    pub x: i64,
-    pub y: i64,
-    pub z: i64,
-}
-
-impl TileId {
-    #[inline]
-    pub fn new(page: usize, x: i64, y: i64, z: i64) -> Self {
-        Self { page, x, y, z }
-    }
-
-    #[inline]
-    pub fn xy(&self) -> Point2<i64> {
-        point![self.x, self.y]
-    }
-}
-
-pub struct TileRect {
-    pub rect: Bounds<i64>,
-    pub z: i64,
-}
+mod tile;
+pub use tile::{TileId, TileRect};
---
+++ app/src/canvas/render/tile.rs
@@ -0,0 +1,28 @@
+use nalgebra::{point, Point2};
+
+use crate::types::Bounds;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct TileId {
+    pub page: usize,
+    pub x: i64,
+    pub y: i64,
+    pub z: i64,
+}
+
+impl TileId {
+    #[inline]
+    pub fn new(page: usize, x: i64, y: i64, z: i64) -> Self {
+        Self { page, x, y, z }
+    }
+
+    #[inline]
+    pub fn xy(&self) -> Point2<i64> {
+        point![self.x, self.y]
+    }
+}
+
+pub struct TileRect {
+    pub rect: Bounds<i64>,
+    pub z: i64,
+}
